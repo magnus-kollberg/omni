dcc0513c798eb6f7970e364cd7193784e00220e8
Magnus Kollberg <magnus.kollberg@semcon.com>
2025-03-22 16:57:02 +0100

    Initial Commit

---
+++ omni/omniApp.swift
@@ -0,0 +1,17 @@
+//
+//  omniApp.swift
+//  omni
+//
+//  Created by Magnus Kollberg on 2025-03-22.
+//
+
+import SwiftUI
+
+@main
+struct omniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
